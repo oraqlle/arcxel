8371bc4a4e945b3aa9753ccb296ab23139e6021d
oraqlle <oraqlle@proton.me>
2026-07-30 01:41:11 +1000

    win: Basic window management

---
+++ include/window.h
@@ -0,0 +1,53 @@
+// <window.h> -*- C++ -*-
+
+//  Arcxel Test Bench
+//  Copyright (C) 2026  Tyler Swann, Georgia Kannelis
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License v2.1 as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//  USA
+
+#ifndef ARCXEL_WINDOW_H
+#define ARCXEL_WINDOW_H
+
+#include <raylib.h>
+
+#include <string>
+
+namespace arcxel {
+
+class Window {
+private:
+    int width;
+    int height;
+    int target_fps;
+    std::string window_name;
+
+public:
+    explicit Window(
+        int w, int h, std::string win_name = "raylib window", int target_fps = 60
+    )
+        : width(w)
+        , height(h)
+        , target_fps(target_fps)
+        , window_name(win_name) {
+        InitWindow(w, h, window_name.c_str());
+        SetTargetFPS(target_fps);
+    }
+
+    ~Window() noexcept { CloseWindow(); }
+};
+
+} // namespace arcxel
+
+#endif // ARCXEL_WINDOW_H
--- src/main.cxx
+++ src/main.cxx
@@ -1,6 +1,19 @@
+#include <window.h>
+
+#include "raylib.h"
+
 #include <print>
 
 auto main() -> int {
     std::println("Hello, World!");
+
+    auto window = arcxel::Window(800, 600, "Arcxel Window");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        EndDrawing();
+    }
+
     return 0;
 }
