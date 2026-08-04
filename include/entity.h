1eddad31b2f16a40905fbb7a334014f2634b0331
oraqlle <oraqlle@proton.me>
2026-08-04 16:32:46 +1000

    wip: Game entity type for ECS

---
+++ include/entity.h
@@ -0,0 +1,49 @@
+// <entity.h> -*- C++ -*-
+
+//  Arcxel Test Bench
+//  Copyright (C) 2026  Tyler Swann, Georgia Kannelis
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License v2.1 as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//  USA
+
+#ifndef ARCXEL_ENTITY_H
+#define ARCXEL_ENTITY_H
+
+#include <raylib.h>
+
+#include <bitset>
+#include <cstdint>
+
+namespace arcxel {
+
+/*** Entity ***/
+
+/**
+ * @brief Entity ID type
+ */
+using Entity = std::uint32_t;
+
+constexpr Entity MAX_ENTITIES = 5000;
+
+/*** Components ***/
+
+using ComponentType = std::uint8_t;
+
+constexpr ComponentType MAX_COMPONENTS = 32;
+
+using Signature = std::bitset<MAX_COMPONENTS>;
+
+} // namespace arcxel
+
+#endif // ARCXEL_ENTITY_H
