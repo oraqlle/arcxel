4793f59ab5a1cadd051ce4fbcbc844f666521a79
oraqlle <oraqlle@proton.me>
2026-08-05 13:16:01 +1000

    feat: Type alias for fixed width integer types and float types

---
+++ include/types.h
@@ -0,0 +1,46 @@
+// <types.h> -*- C++ -*-
+
+//  Arcxel Test Bench
+//  Copyright (C) 2026  Tyler Swann, Georgia Kannelis
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License v2.1 as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//  USA
+
+#ifndef ARCXEL_TYPES_H
+#define ARCXEL_TYPES_H
+
+#include <cstddef>
+#include <cstdint>
+
+namespace arcxel {
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+using usize = size_t;
+using isize = std::intmax_t;
+
+using f32 = float;
+using f64 = double;
+
+} // namespace arcxel
+
+#endif // ARCXEL_TYPES_H
