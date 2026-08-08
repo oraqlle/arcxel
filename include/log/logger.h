4fea9e8f85ca66191a9f273cf62b050cb62d3391
Georgia Kanellis <gkan0011@student.monash.edu>
2026-08-08 10:41:06 +1000

    log: Add missing logger sources

---
+++ include/log/logger.h
@@ -0,0 +1,115 @@
+// <log/logger.h> -*- C++ -*-
+
+//  Arcxel Test Bench
+//  Copyright (C) 2026  Tyler Swann, Georgia Kannelis
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License v2.1 as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//  USA
+
+#ifndef ARCXEL_LOG_LOGGER_H
+#define ARCXEL_LOG_LOGGER_H
+
+#include <log/level.h>
+
+#include <format>
+#include <string_view>
+#include <utility>
+
+// Calls below this level are discarded at compile time. Release keeps info and
+// above so trace and debug cost nothing in a measured build.
+#ifndef ARCXEL_LOG_MIN_LEVEL
+#    ifdef NDEBUG
+#        define ARCXEL_LOG_MIN_LEVEL 2
+#    else
+#        define ARCXEL_LOG_MIN_LEVEL 0
+#    endif
+#endif
+
+namespace arcxel::log {
+
+auto to_string(Level level) noexcept -> const char*;
+
+auto set_level(Level level) noexcept -> void;
+auto level() noexcept -> Level;
+
+// Redirect raylib's own TraceLog output through this logger. Call before
+// InitWindow so raylib's start-up messages are captured too.
+auto adopt_raylib() noexcept -> void;
+
+namespace detail {
+
+auto write(Level level, std::string_view message) -> void;
+
+template <Level L>
+inline constexpr bool compiled_in = static_cast<int>(L) >= ARCXEL_LOG_MIN_LEVEL;
+
+} // namespace detail
+
+template <typename... Args>
+auto trace(std::format_string<Args...> fmt, Args&&... args) -> void {
+    if constexpr (detail::compiled_in<Level::trace>) {
+        if (Level::trace >= level()) {
+            detail::write(Level::trace, std::format(fmt, std::forward<Args>(args)...));
+        }
+    }
+}
+
+template <typename... Args>
+auto debug(std::format_string<Args...> fmt, Args&&... args) -> void {
+    if constexpr (detail::compiled_in<Level::debug>) {
+        if (Level::debug >= level()) {
+            detail::write(Level::debug, std::format(fmt, std::forward<Args>(args)...));
+        }
+    }
+}
+
+template <typename... Args>
+auto info(std::format_string<Args...> fmt, Args&&... args) -> void {
+    if constexpr (detail::compiled_in<Level::info>) {
+        if (Level::info >= level()) {
+            detail::write(Level::info, std::format(fmt, std::forward<Args>(args)...));
+        }
+    }
+}
+
+template <typename... Args>
+auto warn(std::format_string<Args...> fmt, Args&&... args) -> void {
+    if constexpr (detail::compiled_in<Level::warn>) {
+        if (Level::warn >= level()) {
+            detail::write(Level::warn, std::format(fmt, std::forward<Args>(args)...));
+        }
+    }
+}
+
+template <typename... Args>
+auto error(std::format_string<Args...> fmt, Args&&... args) -> void {
+    if constexpr (detail::compiled_in<Level::error>) {
+        if (Level::error >= level()) {
+            detail::write(Level::error, std::format(fmt, std::forward<Args>(args)...));
+        }
+    }
+}
+
+template <typename... Args>
+auto fatal(std::format_string<Args...> fmt, Args&&... args) -> void {
+    if constexpr (detail::compiled_in<Level::fatal>) {
+        if (Level::fatal >= level()) {
+            detail::write(Level::fatal, std::format(fmt, std::forward<Args>(args)...));
+        }
+    }
+}
+
+} // namespace arcxel::log
+
+#endif // ARCXEL_LOG_LOGGER_H
---
+++ src/log/logger.cxx
@@ -0,0 +1,113 @@
+// <log/logger.cxx> -*- C++ -*-
+
+//  Arcxel Test Bench
+//  Copyright (C) 2026  Tyler Swann, Georgia Kannelis
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License v2.1 as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//  USA
+
+#include <log/logger.h>
+
+#include <log/level.h>
+
+#include <raylib.h>
+
+#include <atomic>
+#include <chrono>
+#include <cstdarg>
+#include <cstdio>
+#include <print>
+#include <string_view>
+
+namespace arcxel::log {
+
+namespace {
+
+std::atomic<Level> current_level{Level::info};
+
+auto from_raylib(int raylib_level) noexcept -> Level {
+    switch (raylib_level) {
+    case LOG_TRACE: return Level::trace;
+    case LOG_DEBUG: return Level::debug;
+    case LOG_INFO: return Level::info;
+    case LOG_WARNING: return Level::warn;
+    case LOG_ERROR: return Level::error;
+    case LOG_FATAL: return Level::fatal;
+    default: return Level::info;
+    }
+}
+
+// raylib supplies a printf-style format and a va_list, so the message has to be
+// rendered here before it can join the std::format path. Nothing may escape:
+// this is called from C.
+auto raylib_callback(int raylib_level, const char* text, va_list args) noexcept
+    -> void {
+    try {
+        char buffer[1024];
+
+        if (std::vsnprintf(buffer, sizeof(buffer), text, args) < 0) {
+            return;
+        }
+
+        const auto lvl = from_raylib(raylib_level);
+
+        if (lvl >= level()) {
+            detail::write(lvl, std::string_view(buffer));
+        }
+    } catch (...) {
+    }
+}
+
+} // namespace
+
+auto to_string(Level lvl) noexcept -> const char* {
+    switch (lvl) {
+    case Level::trace: return "TRACE";
+    case Level::debug: return "DEBUG";
+    case Level::info: return "INFO";
+    case Level::warn: return "WARN";
+    case Level::error: return "ERROR";
+    case Level::fatal: return "FATAL";
+    case Level::off: return "OFF";
+    }
+
+    return "?";
+}
+
+auto set_level(Level lvl) noexcept -> void {
+    current_level.store(lvl, std::memory_order_relaxed);
+}
+
+auto level() noexcept -> Level {
+    return current_level.load(std::memory_order_relaxed);
+}
+
+auto adopt_raylib() noexcept -> void {
+    // Let everything through raylib's own filter; ours decides what is kept.
+    SetTraceLogLevel(LOG_ALL);
+    SetTraceLogCallback(raylib_callback);
+}
+
+namespace detail {
+
+auto write(Level lvl, std::string_view message) -> void {
+    const auto now =
+        std::chrono::floor<std::chrono::milliseconds>(std::chrono::system_clock::now());
+
+    std::println(stderr, "[{:%H:%M:%S}] {:<5} {}", now, to_string(lvl), message);
+}
+
+} // namespace detail
+
+} // namespace arcxel::log
